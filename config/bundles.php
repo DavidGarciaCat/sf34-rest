6fbe2748c0df359aa872663af6edb113b5c394d7
David Garcia <david.garcia@deralia.com>
2018-02-22 19:47:39 +0000

    composer require twig
    https://github.com/FriendsOfSymfony/FOSRestBundle/issues/1812#issuecomment-349930668

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
